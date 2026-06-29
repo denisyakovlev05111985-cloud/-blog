841e3093e0f237c02ee7a6b1291e137ad362fbbf
denisyakovlev05111985-cloud <denisyakovlev05111985@gmail.com>
2026-06-29 20:40:29 +0300

    CW: Модуль20

---
+++ views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def post_detail(request):
+    return render(request, 'post_detail.html')
+
+def users_login(request):
+    return render(request, 'users/login.html')
+
+def users_register(request):
+    return render(request, 'users/register.html')
+
+def post_form(request):
+    return render(request, 'posts/post_form.html')
+
+def favorite_list(request):
+    return render(request, 'posts/favorite_list.html')
